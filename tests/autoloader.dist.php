f0c8fe2fe66fdc218f8ee56adce962cbe1b21037
Jakub Zalas <jakub@zalas.pl>
2012-05-13 21:41:00 +0100

    Added composer.json for composer dependency manager. In tests, using autoloader from composer if vendors were installed with it.

--- tests/autoloader.dist.php
+++ tests/autoloader.dist.php
@@ -9,23 +9,29 @@
  * file that was distributed with this source code.
  */
 
-require_once __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-require_once __DIR__ . '/../lib/Vendor/mockery/library/Mockery/Loader.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    // use vendors installed with composer
+    require_once __DIR__ . '/../vendor/autoload.php';
+} else {
+    // use vendors installed as git submodules
+    require_once __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+    require_once __DIR__ . '/../lib/Vendor/mockery/library/Mockery/Loader.php';
 
-$loader = new \Mockery\Loader;
-$loader->register();
+    $loader = new \Mockery\Loader;
+    $loader->register();
 
-$phaLoader= new \Doctrine\Common\ClassLoader('Phabric', __DIR__ . '/../lib');
-$phaLoader->register();
+    $phaLoader= new \Doctrine\Common\ClassLoader('Phabric', __DIR__ . '/../lib');
+    $phaLoader->register();
 
-$docLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../lib/Vendor/Doctrine/lib');
-$docLoader->register();
+    $docLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../lib/Vendor/Doctrine/lib');
+    $docLoader->register();
 
-$docComLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib');
-$docComLoader->register();
+    $docComLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib');
+    $docComLoader->register();
 
-$gherkinLoader = new \Doctrine\Common\ClassLoader('Behat\Gherkin', __DIR__ . '/../vendor/Behat2/vendor/Gherkin/src');
-$gherkinLoader->register();
+    $gherkinLoader = new \Doctrine\Common\ClassLoader('Behat\Gherkin', __DIR__ . '/../vendor/Behat2/vendor/Gherkin/src');
+    $gherkinLoader->register();
 
-$behatLoader = new \Doctrine\Common\ClassLoader('Behat', __DIR__ . '/../vendor/Behat2/src');
-$behatLoader->register();
+    $behatLoader = new \Doctrine\Common\ClassLoader('Behat', __DIR__ . '/../vendor/Behat2/src');
+    $behatLoader->register();
+}
