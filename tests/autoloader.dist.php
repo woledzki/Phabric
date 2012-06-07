5e15867ef4360269b6e964b26e0daf334680f7d4
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-06-07 12:25:48 +0100

    fixing build.xml + small cleanup in autoloader

--- tests/autoloader.dist.php
+++ tests/autoloader.dist.php
@@ -9,29 +9,10 @@
  * file that was distributed with this source code.
  */
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+$vendorDir = realpath(__DIR__ . '/../vendor/');
+if (file_exists("{$vendorDir}/autoload.php")) {
     // use vendors installed with composer
-    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once "{$vendorDir}/autoload.php";
 } else {
-    // use vendors installed as git submodules
-    require_once __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-    require_once __DIR__ . '/../lib/Vendor/mockery/library/Mockery/Loader.php';
-
-    $loader = new \Mockery\Loader;
-    $loader->register();
-
-    $phaLoader= new \Doctrine\Common\ClassLoader('Phabric', __DIR__ . '/../lib');
-    $phaLoader->register();
-
-    $docLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../lib/Vendor/Doctrine/lib');
-    $docLoader->register();
-
-    $docComLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../lib/Vendor/Doctrine/lib/vendor/doctrine-common/lib');
-    $docComLoader->register();
-
-    $gherkinLoader = new \Doctrine\Common\ClassLoader('Behat\Gherkin', __DIR__ . '/../vendor/Behat2/vendor/Gherkin/src');
-    $gherkinLoader->register();
-
-    $behatLoader = new \Doctrine\Common\ClassLoader('Behat', __DIR__ . '/../vendor/Behat2/src');
-    $behatLoader->register();
+    throw new Exception("`autoload.php` not found in `{$vendorDir}`");
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Phabric.
  * (c) Ben Waine <ben@ben-waine.co.uk>
@@ -7,11 +6,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-// Mockery Autoloader required mockery to be on the include path.
-
-set_include_path(implode(PATH_SEPARATOR, array(get_include_path(),
-        __DIR__ . '/../lib/Vendor/mockery/library/')));
 
 date_default_timezone_set('UTC');
 
